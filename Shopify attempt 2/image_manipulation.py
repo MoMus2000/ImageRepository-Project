8baae10bc2bd3ce4a1c1d76b0aa28023cb374c59
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2020-12-22 11:33:17 -0500

    Add files via upload

--- Shopify attempt 2/image_manipulation.py
+++ Shopify attempt 2/image_manipulation.py
@@ -10,14 +10,17 @@ from io import BytesIO
 
 image_manipulation_blueprint = Blueprint('image_manipulation_blueprint',__name__)
 
-"""
-Method: Resize an Image
-Request: POST
-USE : Upload Image, Resize to your liking and download directly
-"""
+
 
 @image_manipulation_blueprint.route('/image/resize/<height>/<width>', methods=['POST'])
 def image_resize(height,width):
+	"""
+	Method: Resize an Image
+	<br>
+	Request: POST
+	<br>
+	USE : Upload Image, Resize to your liking and download directly
+	"""
 	img = Image.open(request.files.get('image').stream)
 	img = img.resize((int(width),int(height)), Image.ANTIALIAS)
 	img_io = BytesIO()
@@ -29,14 +32,17 @@ def image_resize(height,width):
 	img_io.seek(0)
 	return send_file(img_io, as_attachment=True,mimetype='image/jpeg',attachment_filename='logo.png'),201
 
-"""
-Method: grayscale an Image
-Request: POST
-USE : Upload Image, grayscale and download directly
-"""
+
 
 @image_manipulation_blueprint.route('/image/gray', methods=['POST'])
 def image_grayscale():
+	"""
+	Method: grayscale an Image
+	<br>
+	Request: POST
+	<br>
+	USE : Upload Image, grayscale and download directly
+	"""
 	img = Image.open(request.files.get('image').stream).convert('L')
 	img_io = BytesIO()
 	if (img.mode == "JPEG"):
@@ -49,14 +55,17 @@ def image_grayscale():
 	img_io.seek(0)
 	return send_file(img_io, as_attachment=True,mimetype='image/jpeg',attachment_filename='logo.png'),201
 
-"""
-Method: change quality of an Image
-Request: POST
-USE : Upload Image, change quality and download directly
-"""
+
 
 @image_manipulation_blueprint.route('/image/quality/<percent>', methods=['POST'])
 def image_quality(percent):
+	"""
+	Method: change quality of an Image
+	<br>
+	Request: POST
+	<br>
+	USE : Upload Image, change quality and download directly
+	"""
 	img = Image.open(request.files.get('image').stream)
 	img_io = BytesIO()
 	if (img.mode == "JPEG"):
@@ -69,14 +78,17 @@ def image_quality(percent):
 	img_io.seek(0)
 	return send_file(img_io, as_attachment=True,mimetype='image/jpeg',attachment_filename='logo.png'),201
 
-"""
-Method: change contrast of an Image
-Request: POST
-USE : Upload Image, change contrast and download directly
-"""
+
 
 @image_manipulation_blueprint.route('/image/contrast/<value>', methods=['POST'])
 def image_contrast(value):
+	"""
+	Method: change contrast of an Image
+	<br>
+	Request: POST
+	<br>
+	USE : Upload Image, change contrast and download directly
+	"""
 	img = Image.open(request.files.get('image').stream)
 	contrast = ImageEnhance.Contrast(img)
 	img = contrast.enhance(int(value))
