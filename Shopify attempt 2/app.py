7593959750f55be8e71792d6bfcd6bb7db130000
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2020-12-22 11:32:17 -0500

    Add files via upload

--- Shopify attempt 2/app.py
+++ Shopify attempt 2/app.py
@@ -6,18 +6,16 @@ from upload import upload_blueprint
 from image import image_blueprint
 from image_manipulation import image_manipulation_blueprint
 
-
+app = Flask(__name__)
 """
-Api Service Written in flask using firebase for authentication and database. 
+Api Service Written in flask using firebase. 
+<br>
 Author: Mustafa Muhammad
+<br>
 For : Shopify Technical Interview
+<br>
 Date : 21 December 2020
 """
-
-
-
-
-app = Flask(__name__)
 app.secret_key = os.urandom(10)
 app.register_blueprint(login_blueprint)
 app.register_blueprint(signup_blueprint)
@@ -25,7 +23,8 @@ app.register_blueprint(upload_blueprint)
 app.register_blueprint(image_blueprint)
 app.register_blueprint(image_manipulation_blueprint)
 
-app.run(debug = True)
+if __name__ == "__main__":
+	app.run(debug = True)
 
 
 
