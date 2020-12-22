cb709a4544a2a383822d3933afa33a4cb67a01e8
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2020-12-22 11:32:40 -0500

    Add files via upload

--- Shopify attempt 2/constant.py
+++ Shopify attempt 2/constant.py
@@ -1,12 +1,3 @@
-
-"""
-Make the code cleaner by hiding the api keys and urls to a different file.
-Initially wanted to create environment variables but due to the nature of the project,
-would not have been able to be checked and accessed by the shopify interviewer.
-"""
-
-
-
 config = {
 	"apiKey": "AIzaSyAhrOEolzpYMXYit0LWWhxmSC9zMieo6TM",
     "authDomain": "shopifyproject-e8627.firebaseapp.com",
@@ -16,3 +7,9 @@ config = {
     "messagingSenderId": "771306858953",
     "appId": "1:771306858953:web:010b5ce5392cf40d5b6018"
 }
+
+"""
+Make the code cleaner by hiding the api keys and urls to a different file.
+Initially wanted to create environment variables but due to the nature of the project,
+would not have been able to be checked and accessed by the shopify interviewer.
+"""
